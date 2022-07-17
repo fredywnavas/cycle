4f74b46b9ab38915e3080904384e2852195eada9
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-17 18:32:57 -0500

    Constants completed.

--- constants.py
+++ constants.py
@@ -13,3 +13,9 @@ WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 YELLOW = Color(255, 255, 0)
 GREEN = Color(0, 255, 0)
+
+# x component from where the score is displayed
+SCORE_X = 350
+
+# y component from where the score is displayed
+SCORE_Y = 0
