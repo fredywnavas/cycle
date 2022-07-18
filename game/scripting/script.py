219defa9cb70e4e641b6896be5191d43479f5bf1
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 03:24:20 -0500

    adding script class

---
+++ game/scripting/script.py
@@ -0,0 +1,7 @@
+class Script:
+    """
+    A collection of actions.
+
+    The responsibility of Script is to keep track of a collection of actions.
+    It has methods for adding, removing and getting them by a group name.
+    """
