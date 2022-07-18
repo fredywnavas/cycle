69e5edcaeb44b023a7441a019d3ed35b7f6a2b3e
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-17 23:42:27 -0500

    finished actor

--- game/casting/cast.py
+++ game/casting/cast.py
@@ -72,3 +72,13 @@ class Cast:
             results = self._actors[group][0]
         return result
         
+    def remove_actor(self, group, actor):
+        """
+        Removes an actor from the given group.
+
+        Args:
+            group (string): The name of the group.
+            actor (Actor): The actor to remove.
+        """
+        if group in self._actors:
+            self._actors[group].remove(actor)
