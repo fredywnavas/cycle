14dd3e9ffd12ab54f6cd8b7ef5760f38886fc775
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 01:58:52 -0500

    action corrected

--- game/scripting/action.py
+++ game/scripting/action.py
@@ -5,7 +5,7 @@ class Action:
     The responsibility of action is to do something that is integral or important in the game.
     Thus, it has one method, execute(), which should be overridden by derived classes.
     """
-    def execute(self, cast, script)
+    def execute(self, cast, script):
         """
         Executes something that is important in the game.
         This method should be overridden by derived classes.
