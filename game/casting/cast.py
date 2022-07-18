27231c8328b202a33cec404d244fd1145a82fbb6
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 05:12:41 -0500

    debug cast

--- game/casting/cast.py
+++ game/casting/cast.py
@@ -69,7 +69,7 @@ class Cast:
         """
         result = None
         if group in self._actors.keys():
-            results = self._actors[group][0]
+            result = self._actors[group][0]
         return result
         
     def remove_actor(self, group, actor):
