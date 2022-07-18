3ed1a747f8387dc9015e2f168efb234c47451f0a
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 03:27:59 -0500

    updated script

--- game/scripting/script.py
+++ game/scripting/script.py
@@ -4,4 +4,23 @@ class Script:
 
     The responsibility of Script is to keep track of a collection of actions.
     It has methods for adding, removing and getting them by a group name.
+
+    Attributes:
+        _actions (dict): A dictionary of actions { key: group_name, value: a list of actions }
     """
+    def __init__(self):
+        """
+        Constructs a new Action.
+        """
+        self._actions = {}
+
+    def add_action(self, group, action):
+        """
+        Adds an action to the given group.
+
+        Args:
+            group (string): The name of the group.
+            action (Action): The action to add.
+        """
+        if not group in self._actions.keys():
+            self._actions[group] = []
