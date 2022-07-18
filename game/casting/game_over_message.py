3376f572bd4c86fa6ec7388c8dbb79dbe9456d92
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 00:28:33 -0500

    finished game over message

--- game/casting/game_over_message.py
+++ game/casting/game_over_message.py
@@ -3,5 +3,32 @@ from game.casting.actor import Actor
 
 class GameOver(Actor):
     """
-    
+    Displays a Game Over Message.
+
+    Attributes:
+        _color (constant): The color value the game over message is displayed in.
     """
+    def __init__(self):
+        """
+        Constructs a game over message.
+        """
+        super().__init__()
+        self._color = constants.GREEN
+
+    def get_color(self):
+        """
+        Gets the actor's color as a tuple of three ints (r, g, b).
+        
+        Returns:
+            Color: The actor's text color.
+        """
+        return super().get_color()
+
+    def set_color(self, color):
+        """
+        Updates the color to the given one.
+
+        Args:
+            color (Color): The given color.
+        """
+        return super().set_color(color)
