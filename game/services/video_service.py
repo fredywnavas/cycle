fc899035422000e0ba9d810ab0f4a4c8bdbc10c8
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-18 04:14:56 -0500

    video service completed

--- game/services/video_service.py
+++ game/services/video_service.py
@@ -14,3 +14,96 @@ class VideoService:
             debug (bool): whether or noto to draw in debug mode.
         """
         self._debug = debug
+
+    def close_window(self):
+        """
+        Closes the window and releases all computing resources.
+        """
+        pyray.close_window()
+
+    def clear_buffer(self):
+        """
+        Clears the buffer in preparation for the next rendering.
+        This method should be called at the beginningn of the game's output phase.
+        """
+        pyray.begin_drawing()
+        pyray.clear_background(pyray.BLACK)
+        if self._debug == True:
+            self._draw_grid()
+
+    def draw_actor(self, actor, centered = False):
+        """
+        Draws the given actor's text on the screen.
+
+        Args:
+            actor (Actor): The actor to draw.
+        """
+        text = actor.get_text()
+        x = actor.get_position().get_x()
+        y = actor.get_position().get_y()
+        font_size = actor.get_font_size()
+        color = actor.get_color().to_tuple()
+
+        if centered:
+            width = pyray.measure_text(text, font_size)
+            offset = int(width / 2)
+            x -= offset
+
+        pyray.draw_text(text, x, y, font_size, color)
+
+    def draw_actors(self, actors, centered = False):
+        """
+        Draws the text for the given list of actors on the screen.
+        
+        Args:
+            actors (list): A list of actors to draw.
+        """
+        for actor in actors:
+            self.draw_actor(actor, centered)
+
+    def flush_buffer(self):
+        """
+        Copies the buffer contents to the screen.
+        This method should be called at the end of teh game's output phase.
+        """
+        pyray.end_drawing()
+
+    def is_window_open(self):
+        """
+        Whether or not the window was closed by the user.
+
+        Returns:
+            bool: True if the window is closing; False if otherwise.
+        """
+        return not pyray.window_should_close()
+    
+    def open_window(self):
+        """
+        OPens a new windo with the provided title.
+
+        Args:
+            title (string): The title of the window.
+        """
+        pyray.init_window(constants.MAX_X, constants.MAX_Y, constants.CAPTION)
+        pyray.set_target_fps(constants.FRAME_RATE)
+
+    def _draw_grid(self):
+        """
+        Draws a grid on the screen.
+        """
+        for y in range(0, constants.MAX_Y, constants.CELL_SIZE):
+            pyray.draw_line(0, y, constants.MAX_X, y, pyray.GRAY)
+
+        for x in range(0, constants.MAX_X, constants.CELL_SIZE):
+            pyray.draw_line(x, 0, x, constants.MAX_Y, pyray.GRAY)
+
+    def _get_x_offset(self, text, font_size):
+        """
+        Gets the offset measurement for the text.
+
+        Args:
+            text (string)           : The actor's textual representation.
+            font_size (constant)    : The size of an actor.
+        """
+        width = pyray.measure_text(text, font_size)
+        return int(width / 2)
